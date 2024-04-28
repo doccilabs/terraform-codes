ca6c06c739bfbf05af6fd9f5ffe85abc88e5fab9
BrianDYKim <18k7102dy@gmail.com>
2024-04-28 22:43:38 +0900

    보안상 제거할 파일 삭제

--- modules/dual-stack-vpc/vpc.tf
+++ modules/dual-stack-vpc/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.cidr_block_ipv4
   ipv6_cidr_block  = var.cidr_block_ipv6
-  ipv6_ipam_pool_id = aws_vpc_ipam_pool.ipv6_pool.id
+  ipv6_ipam_pool_id = var.ipv6_ipam_pool_id
   instance_tenancy = var.instance_tenancy
 
   depends_on = [aws_vpc_ipam_pool.ipv6_pool]
