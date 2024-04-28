7d2d38b4d12bd17310b344422f3450e6d1a7c5d4
BrianDYKim <18k7102dy@gmail.com>
2024-04-28 22:38:33 +0900

    ipam module 정의 완료

---
+++ modules/ipv6_ipam/ipam_ipv6_pool.tf
@@ -0,0 +1,37 @@
+data "aws_region" "current" {
+
+}
+
+resource "aws_vpc_ipam" "main" {
+  operating_regions {
+    region_name = data.aws_region.current.name
+  }
+
+  cascade = true
+  description = var.vpc_ipam_description
+}
+
+resource "aws_vpc_ipam_pool" "ipv6_pool" {
+  ipam_scope_id = aws_vpc_ipam.main.public_default_scope_id
+  aws_service = "ec2"
+  locale        = data.aws_region.current.name
+  address_family = "ipv6"
+  description = var.ipam_pool_description
+
+  allocation_default_netmask_length = var.allocation_default_netmask_length
+  allocation_max_netmask_length = var.allocation_max_netmask_length
+  allocation_min_netmask_length = var.allocation_min_netmask_length
+
+  publicly_advertisable = true
+  public_ip_source = "amazon"
+
+  tags = merge(
+    local.common_tags,
+    var.tags
+  )
+}
+
+resource "aws_vpc_ipam_pool_cidr" "ipv6" {
+  ipam_pool_id = aws_vpc_ipam_pool.ipv6_pool.id
+  netmask_length = 52
+}
---
+++ modules/vpc/vpc.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "main" {
+  cidr_block       = var.cidr_block_ipv4
+  ipv6_cidr_block  = var.cidr_block_ipv6
+  ipv6_ipam_pool_id = aws_vpc_ipam_pool.ipv6_pool.id
+  instance_tenancy = var.instance_tenancy
+
+  depends_on = [aws_vpc_ipam_pool.ipv6_pool]
+
+  tags = merge(
+    local.common_tags,
+    var.tags
+  )
+}
